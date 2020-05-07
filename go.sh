c25029b3702b18e2cc9c36aba311f78e61d78e80
rahulwaykos <60727515+rahulwaykos@users.noreply.github.com>
2020-05-07 21:55:45 +0530

    Create go.sh

---
+++ go.sh
@@ -0,0 +1,10 @@
+apt install wget
+cd /usr/local/
+
+wget https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz
+
+tar -C /usr/local -xzf go1.14.2.linux-amd64.tar.gz
+
+export PATH=$PATH:/usr/local/go/bin
+
+source $HOME/.profile
