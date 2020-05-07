f2ed825e87ec8416dfc80d05302618b1ae8313e9
rahulwaykos <60727515+rahulwaykos@users.noreply.github.com>
2020-05-07 21:33:33 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,24 @@
+git clone https://github.com/play-with-docker/play-with-docker
+cd play-with-docker
+
+# Verify the Docker daemon is running
+docker run hello-world
+
+# Load the IPVS kernel module. Because swarms are created in dind,
+# the daemon won't load it automatically
+sudo modprobe xt_ipvs
+
+# Ensure Docker daemon is running in swarm mode
+docker swarm init
+
+# Get the latest franela/dind image
+docker pull franela/dind
+
+# Optional (with go1.14): pre-fetch module requirements into vendor
+# so that no network requests are required within the containers.
+# The module cache is retained in the pwd and l2 containers so the
+# download is a one-off if you omit this step.
+go mod vendor
+
+# Start PWD as a container
+docker-compose up
